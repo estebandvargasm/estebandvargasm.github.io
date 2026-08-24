f055836c80ed573c124e475dadfe9a13958e7d6a
estebandvargasm <estebandvargasm@outlook.com>
2026-08-23 23:34:50 -0500

    style: make ripple grid slightly more visible

--- src/components/ui/background-ripple-effect.jsx
+++ src/components/ui/background-ripple-effect.jsx
@@ -93,7 +93,7 @@ const DivGrid = ({
           <div
             key={idx}
             className={cn(
-              "cell relative border-[0.5px] opacity-20 transition-opacity duration-150 will-change-transform hover:opacity-50 dark:shadow-[0px_0px_40px_1px_var(--cell-shadow-color)_inset]",
+              "cell relative border-[0.5px] opacity-40 transition-opacity duration-150 will-change-transform hover:opacity-70 dark:shadow-[0px_0px_40px_1px_var(--cell-shadow-color)_inset]",
               clickedCell && "animate-cell-ripple [animation-fill-mode:none]",
               !interactive && "pointer-events-none"
             )}
